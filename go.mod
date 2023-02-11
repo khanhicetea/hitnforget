755545de922ea286599fa601ab58e330d4e1c181
KhanhIceTea <khanhicetea@gmail.com>
2023-02-11 14:13:08 +0700

    finalize

--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.18
 
 require (
 	github.com/go-redis/redis/v8 v8.11.5
-	github.com/urfave/cli/v2 v2.5.1
+	github.com/urfave/cli/v2 v2.24.3
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,188 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
-	"github.com/khanhicetea/hitnforget/server"
-	"github.com/khanhicetea/hitnforget/worker"
+	"github.com/go-redis/redis/v8"
 	cli "github.com/urfave/cli/v2"
 )
+
+func matchPattern(pattern string, s string) bool {
+	if pattern == "*" {
+		return true
+	}
+	matched, _ := filepath.Match(pattern, s)
+	return matched
+}
+
+func newRedis(ctx *cli.Context) (*redis.Client, context.Context) {
+	redisAddr := ctx.Value("redis").(string)
+
+	return redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	}), context.Background()
+}
+
+func queueRedisKey(queueName string) string {
+	return fmt.Sprintf("hnf:queue:%s", queueName)
+}
+
+func queueRedisChannel(queueName string) string {
+	return fmt.Sprintf("hnf:channel:%s", queueName)
+}
+
+func queueRequest(r *http.Request, rdb *redis.Client, ctx context.Context, queueName string) (int64, error) {
+	dr, _ := httputil.DumpRequest(r, true)
+
+	rID, err := rdb.Incr(ctx, "hnf:requests_num").Result()
+	if err != nil {
+		return 0, err
+	}
+
+	err = rdb.Set(ctx, "hnf:request:"+fmt.Sprint(rID), dr, 0).Err()
+	if err != nil {
+		return 0, err
+	}
+
+	err = rdb.RPush(ctx, queueName, rID).Err()
+	if err != nil {
+		return 0, err
+	}
+
+	return rID, nil
+}
+
+func getRequestFromQueue(rID string, rdb *redis.Client, ctx context.Context) (*http.Request, error) {
+	rawRequest, _ := rdb.Get(ctx, "hnf:request:"+rID).Result()
+	reader := strings.NewReader(rawRequest)
+	buf := bufio.NewReader(reader)
+	req, _ := http.ReadRequest(buf)
+
+	return req, nil
+}
+
+func httpHandler(c *cli.Context) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	rdb, ctx := newRedis(c)
+	defaultChannel := queueRedisChannel("default")
+	pubsub := rdb.Subscribe(ctx, defaultChannel)
+	defer pubsub.Close()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+	mux.HandleFunc("/_queue", func(w http.ResponseWriter, r *http.Request) {
+		rID, err := queueRequest(r, rdb, ctx, "hnf:queue:default")
+		if err != nil {
+			io.WriteString(w, "Error push to queue")
+			return
+		}
+		rdb.Publish(ctx, defaultChannel, rID)
+		io.WriteString(w, fmt.Sprintf("Queued request %d", rID))
+	})
+
+	return mux
+}
+
+func runWorker(c *cli.Context, wId int, workingQueue string, failedQueue string) {
+	fmt.Println("Worker", wId, "is ready to work on queue", workingQueue, "...")
+
+	rdb, ctx := newRedis(c)
+	tr := &http.Transport{
+		MaxIdleConnsPerHost: 30,
+		TLSHandshakeTimeout: time.Minute,
+	}
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   time.Minute,
+	}
+
+	pubsub := rdb.Subscribe(ctx, queueRedisChannel(workingQueue))
+	defer pubsub.Close()
+
+	redisKeyWorkingQueue := queueRedisKey(workingQueue)
+	redisKeyFailedQueue := queueRedisKey(failedQueue)
+
+	for {
+		popRequestID, err := rdb.LPop(ctx, redisKeyWorkingQueue).Result()
+		if err != nil {
+			_, err := pubsub.ReceiveMessage(ctx)
+			if err != nil {
+				time.Sleep(time.Second)
+			}
+			continue
+		}
+
+		fmt.Println("Worker", wId, "is working on request", popRequestID)
+
+		origReq, _ := getRequestFromQueue(popRequestID, rdb, ctx)
+		origUrl := origReq.Header.Get("X-Hnf-Url")
+		origMethod := origReq.Header.Get("X-Hnf-Method")
+		expectedStatusCode := origReq.Header.Get("X-Hnf-Expected-Status")
+
+		if origUrl == "" {
+			rdb.Del(ctx, "hnf:request:"+popRequestID)
+			fmt.Printf("Request %s has no URL, skipping\n", popRequestID)
+			continue
+		}
+
+		if origMethod == "" {
+			origMethod = "GET"
+		}
+		if expectedStatusCode == "" {
+			expectedStatusCode = "2**"
+		}
+
+		doReq, _ := http.NewRequest(origMethod, origUrl, origReq.Body)
+
+		for key, values := range origReq.Header {
+			if !strings.Contains(key, "X-Hnf-") {
+				for _, value := range values {
+					doReq.Header.Add(key, value)
+				}
+			}
+		}
+
+		doReq.Header.Set("Connection", "Close")
+
+		resp, err := client.Do(doReq)
+		if err != nil || !matchPattern(expectedStatusCode, fmt.Sprint(resp.StatusCode)) {
+			if failedQueue != "" {
+				fmt.Println("Delegate to", failedQueue, "the request", popRequestID)
+				err = rdb.RPush(ctx, redisKeyFailedQueue, popRequestID).Err()
+				if err != nil {
+					panic(err)
+				}
+				rdb.Publish(ctx, queueRedisChannel(failedQueue), popRequestID)
+				continue
+			} else {
+				fmt.Println("Drop the request", popRequestID)
+			}
+		}
+
+		rdb.Del(ctx, "hnf:request:"+popRequestID)
+		fmt.Printf("Request %s has status code = %d , deleted #%s requests from queue\n", popRequestID, resp.StatusCode, popRequestID)
+	}
+
+}
 
 func main() {
 	app := &cli.App{
 		Name:        "HitNForget",
 		Description: "HTTP Later Server",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "config",
-				Aliases: []string{"c"},
-				Usage:   "Load configuration from `FILE`",
-			},
-		},
 		Commands: []*cli.Command{
 			{
 				Name:    "server",
@@ -38,14 +197,19 @@ func main() {
 					&cli.StringFlag{
 						Name:  "port",
 						Usage: "Binding port",
-						Value: "3333",
+						Value: "8080",
+					},
+					&cli.StringFlag{
+						Name:  "redis",
+						Usage: "Redis Addr",
+						Value: "127.0.0.1:6379",
 					},
 				},
 				Action: func(c *cli.Context) error {
 					bind := c.Value("bind").(string)
 					port := c.Value("port").(string)
-					fmt.Printf("Running queue server on %s:%s ...", bind, port)
-					http.ListenAndServe(net.JoinHostPort(bind, port), server.HTTPHandler())
+					fmt.Printf("Running QUEUE server on %s:%s ...", bind, port)
+					http.ListenAndServe(net.JoinHostPort(bind, port), httpHandler(c))
 					return nil
 				},
 			},
@@ -64,12 +228,18 @@ func main() {
 						Usage: "Next failed queue",
 						Value: "",
 					},
+					&cli.StringFlag{
+						Name:  "redis",
+						Usage: "Redis Addr",
+						Value: "127.0.0.1:6379",
+					},
 				},
 				Action: func(c *cli.Context) error {
 					workingQueue := c.Value("working_queue").(string)
 					failedQueue := c.Value("failed_queue").(string)
-					fmt.Printf("Running worker on %s and fallback to %s ...", workingQueue, failedQueue)
-					worker.Worker(rand.Intn(100), workingQueue, failedQueue)
+					workerId := rand.Intn(10000)
+					fmt.Printf("Running WORKER #%d on %s and fallback to %s ...", workerId, workingQueue, failedQueue)
+					runWorker(c, workerId, workingQueue, failedQueue)
 					return nil
 				},
 			},
